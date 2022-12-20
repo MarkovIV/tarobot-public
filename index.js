acec8422d18e2f4a74717c3163d266e93b6b089f
Ivan <mrkvvn@yandex.ru>
2022-12-20 15:21:45 +0300

    Testing functionality

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const token = '5827923507:AAENSiL7ZFD1rTGA8nNFwwmNRO_Dy8x_E4A'
 const bot = new TelegramApi(token, {polling: true})
 
 bot.on('message', msg => {
-	console.log(msg)
+	const text = msg.text
+	const chatId = msg.chat.id
+
+	bot.sendMessage(chatId, `Вы ввели сообщение: ${text}`)
 })
