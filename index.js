8f929638031ecbfd19cd889ede75acc6e528ee9c
Ivan <mrkvvn@yandex.ru>
2022-12-20 12:50:17 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramApi = require('node-telegram-bot-api')
+
+const token = '5827923507:AAENSiL7ZFD1rTGA8nNFwwmNRO_Dy8x_E4A'
+
+const bot = new TelegramApi(token, {polling: true})
+
+bot.on('message', msg => {
+	console.log(msg)
+})
